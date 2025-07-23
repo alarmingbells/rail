c07e9d8d52f63264e68718cfc1b10a0bc6886889
Lucas Bellamy <funlucasmoo@icloud.com>
2025-07-23 17:21:20 -0500

    global variables & assign

--- rail_compile.c
+++ rail_compile.c
@@ -5,31 +5,146 @@
 
 int newCall = 1;
 char call[32];
-int line = 1;
+int line = 0;
 int inMain = 0;
+int inFunc = 0;
+
+int currentVarIdx = 0;
+
+int argCount = 0;
+int buffer = 0;
+
+#define MAX_VARS 128
+
+typedef struct {
+    char name[32];
+    int address;
+} Variable;
+
+Variable vars[MAX_VARS];
+int var_count = 0;
+int next_heap_addr = 0x0200;
+
+int add_variable(const char *name) {
+    if (var_count >= MAX_VARS) return -1;
+    strncpy(vars[var_count].name, name, sizeof(vars[var_count].name) - 1);
+    vars[var_count].name[sizeof(vars[var_count].name) - 1] = '\0';
+    vars[var_count].address = next_heap_addr++;
+    return var_count++;
+}
+
+int find_variable(const char *name) {
+    for (int i = 0; i < var_count; i++) {
+        if (strcmp(vars[i].name, name) == 0) return i;
+    }
+    return -1;
+}
+
+const char *rxlib[] = {
+    "print",
+    "A9 01 AE TP TP A0 TP 20 00 FF ", // pointer to string, length of string
+    "input",
+    "A9 02 20 00 FF"
+};
 
 int parseToken(char *token) {
     if (newCall) {
         newCall = 0;
-        strcpy(call, token);
+
+        size_t len = strlen(token);
+        if (len > 0 && token[len - 1] == ';') {
+            strncpy(call, token, len - 1);
+            call[len - 1] = '\0';
+        } else {
+            strcpy(call, token);
+        }
+
         if (strncmp(call, "main", 4) == 0) {
             inMain = 1;
         } else {
             if (!inMain) {
-                printf("\033[31mRail compile failure: call outside of main at line %d\033[0m\n", line);
+                if (strcmp(call, "function") == 0) {
+                    inFunc = 1;
+                }
+                printf("\033[31mRail compile failure: call outside of main or function at line %d\033[0m\n", line);
                 return -1;
             } else if (call[0] == '#') {
-                printf("\033[31mRail compile failure: invalid system call '%s' at line %d\033[0m\n", call, line);
-                return -1;
+                int found = -1;
+                for (size_t i = 0; i < sizeof(rxlib) / sizeof(rxlib[0]); i++) {
+                    if (strcmp(call + 1, rxlib[i]) == 0) {
+                        found = i;
+                        break;
+                    }
+                }
+                if (found != -1) {
+                    printf("%s", rxlib[found+1]);
+                } else {
+                    printf("\n\033[31mRail compile failure: unknown system call '%s' at line %d\033[0m\n", call, line);
+                    return -1;
+                }
+            } else if (strcmp(call, "end") == 0) {
             } else {
-                printf("\033[31mRail compile failure: unknown call '%s' at line %d\033[0m\n", call, line);
+                if ((strcmp(call, "global") == 0)) {
+                } else if ((strcmp(call, "assign") == 0)) {
+                } else {
+                    printf("\n\033[31mRail compile failure: unknown function or keyword '%s' at line %d\033[0m\n", call, line);
+                    return -1;
+                }                
+            }
+        }
+    } else {
+        argCount++;
+
+        char stoken[128];
+
+        size_t len = strlen(token);
+        if (len > 0 && token[len - 1] == ';') {
+            strncpy(stoken, token, len - 1);
+            stoken[len - 1] = '\0';
+        } else {
+            strcpy(stoken, token);
+        }
+
+        if (strcmp(call, "assign") == 0) {
+            if (argCount == 1) {
+                char *endptr;
+                buffer = strtol(stoken, &endptr, 10);
+                if (*endptr != '\0') {
+                    printf("\n\033[31mRail compile failure: expected integer value for 'assign', got '%s' at line %d\033[0m\n", stoken, line);
+                    return -1;
+                }
+            } else if (argCount == 2) {
+                printf("A9 %02X 8D TP TP ", buffer);
+            } else {
+                printf("\n\033[31mRail compile failure: incorrect number of arguments for '%s' at line %d\033[0m\n", call, line);
                 return -1;
             }
-            
+        } else if (strcmp(call, "global") == 0) {
+            if (argCount == 1) {
+                if (find_variable(stoken) == -1) {
+                    currentVarIdx = add_variable(stoken);
+                } else {
+                    printf("\n\033[31mRail compile failure: global name '%s' already in use at line %d\033[0m\n", stoken, line);
+                    return -1;
+                }
+            } else if (argCount == 2) {
+                char *endptr;
+                int value = strtol(stoken, &endptr, 10);
+                if (*endptr != '\0') {
+                    printf("\n\033[31mRail compile failure: expected integer value for 'global', got '%s' at line %d\033[0m\n", stoken, line);
+                    return -1;
+                }
+                printf("A9 %02X 8D ", value);
+                printf("%02X %02X ", vars[currentVarIdx].address & 0xFF, (vars[currentVarIdx].address >> 8) & 0xFF);
+            } else {
+                printf("\n\033[31mRail compile failure: incorrect number of arguments for '%s' at line %d\033[0m\n", call, line);
+                return -1;
+            }
         }
     }
     if (token[strlen(token)-1] == ';' || token[strlen(token)-1] == ':') {
         newCall = 1;
+        argCount = 0;
     }
     return 0;
 }
@@ -66,16 +181,18 @@ void compile(char *code) {
         token[tokenIdx] = '\0';
         parseToken(token);
     }
+    printf("\n\033[32mFile compiled successfully.\033[0m\n");
 }
 
 int main() {
-    printf("Rail Compiler for 6502\n");
+    printf("Rail Compiler version [0.1]\n");
+    printf("Copyright (c) me 2025. All rights reserved.\n");
     while (1) {
         newCall = 1;
         call[32];
         line = 1;
         inMain = 0;
-        printf("RAIL>");
+        printf("\nRAIL>");
 
         char filename[500];
 
@@ -86,7 +203,7 @@ int main() {
             filename[len - 1] = '\0';
         }
         if (strcmp(&filename[len - 5], "rail") != 0) {
-            printf("\033[31mFile open failure: '%s' is not a valid .rail file\033[0m\n", filename);
+            printf("\n\033[31mFile open failure: '%s' is not a valid .rail file\033[0m\n", filename);
             continue;
         }
 
@@ -101,16 +218,17 @@ int main() {
                 size_t bytesRead = fread(content, 1, file_size, file);
                 content[bytesRead] = '\0';
                 fclose(file);
-                printf("Starting build of file '%s'...\n", filename);
+                printf("\nStarting build of file '%s'...\n", filename);
                 compile(content);
                 free(content);
             } else {
-                printf("\033[31mFile open failure: memory allocation failure. Please try again\033[0m\n");
+                printf("\n\033[31mFile open failure: memory allocation failure. Please try again\033[0m\n");
                 fclose(file);
             }
         } else {
-            printf("\033[31mFile open failure: file '%s' was not found\033[0m\n", filename);
+            printf("\n\033[31mFile open failure: file '%s' was not found\033[0m\n", filename);
         }
+        printf("Compilation finished.\n", filename);
     }
 
     return 0;
